bc2f4987edfed737cc86e1b46e8f56ee0ed34029
Antoine-Averland <antoine.averland@edu.esiee.fr>
2024-01-05 19:59:45 +0100

    add the starting procedure and the pause founction on the swich 1

--- script.s
+++ script.s
@@ -51,7 +51,11 @@ GPIO_I_PUR   		EQU 	0x00000510  ; GPIO Pull-Up (p432 datasheet de lm3s9B92.pdf)
 BROCHE4_5			EQU		0x30		; led1 & led2 sur broche 4 et 5
 
 BROCHE6				EQU 	0x40		; bouton poussoir 1
+
+BROCHE7				EQU		0x80		; bonton poussoir 2
 	
+BROCHE6_7			EQU		0xC0		; bonton poussoir 1 et 2
+
 BROCHE0				EQU		0x01		;bumper droit
 
 BROCHE1				EQU		0x02		;bumper gauche
@@ -102,7 +106,7 @@ __main
 		mov r2, #0x000       					;; pour eteindre LED
 		
 		
-		;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CONFIGURATION bumper gauche
+		;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CONFIGURATION bumper droit et gauche
 
 		ldr r7, = GPIO_PORTE_BASE+GPIO_I_PUR	;; Pul_up 
         ldr r0, = BROCHE1_2	
@@ -112,8 +116,47 @@ __main
         ldr r0, = BROCHE1_2	
         str r0, [r7]     
 		
-
 		
+		;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CONFIGURATION Switcher 1 et 2
+
+		;ldr r8, = GPIO_PORTD_BASE+GPIO_I_PUR	;; Pul_up 
+        ;ldr r0, = BROCHE6_7	
+        ;str r0, [r8]
+		
+		;ldr r8, = GPIO_PORTD_BASE+GPIO_O_DEN	;; Enable Digital Function 
+        ;ldr r0, = BROCHE6_7
+        ;str r0, [r8]
+		
+		;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CONFIGURATION Switcher 1 et 2
+
+		;ldr r7, = GPIO_PORTD_BASE+GPIO_I_PUR	;; Pul_up 
+        ;ldr r0, = BROCHE6_7
+        ;str r0, [r7]
+		
+		;ldr r7, = GPIO_PORTD_BASE+GPIO_O_DEN	;; Enable Digital Function 
+        ;ldr r0, = BROCHE6_7	
+        ;str r0, [r7]     
+		
+		;ldr r7, = GPIO_PORTD_BASE + (BROCHE6_7<<2)  ;; @data Register = @base + (mask<<2) ==> Switcher
+		
+		;vvvvvvvvvvvvvvvvvvvvvvvFin configuration Switcher 
+
+		;phase de mise en grille
+		; Activer les deux moteurs droit et gauche
+
+PLACEMENT
+		BL	MOTEUR_DROIT_ON
+		BL	MOTEUR_GAUCHE_ON
+		; Evalbot avance droit devant
+		BL	MOTEUR_DROIT_AVANT
+		BL	MOTEUR_GAUCHE_AVANT
+		BL	WAITmg
+		
+		BL	WAITstart
+		
+		b	PLACEMENT
+
+		;phase de course
 loop	
 		mov r3, #0		;; Allume LED1&2 portF broche 4&5 : 00110000		
 		ldr r6, = GPIO_PORTF_BASE + (BROCHE4_5<<2)  ;; @data Register = @base + (mask<<2) ==> LED1
@@ -149,12 +192,62 @@ BG_actif
 		BL	WAITtourne
 		b	loop
 		
+B_actif_depart
 
+		BL	MOTEUR_GAUCHE_OFF
+		BL	MOTEUR_DROIT_OFF
+		
+		BX	LR
 
+SW1_actif
+		
+		BL	MOTEUR_DROIT_OFF
+		BL	MOTEUR_GAUCHE_OFF
+		BL	WAIToff
+		b	SW1_actif
+		
+SW2_actif
+		ldr	r1, =0xAAAAAA
+		; allumer la led broche 4 et 5 (BROCHE4_5)
+		mov r3, #BROCHE4_5		;; Allume LED1&2 portF broche 4&5 : 00110000		
+		ldr r6, = GPIO_PORTF_BASE + (BROCHE4_5<<2)  ;; @data Register = @base + (mask<<2) ==> LED1
+		str r3, [r6]		;; Allume LED1&2 portF broche 4&5 : 00110000 (contenu de r3)
+wait4	
+		subs r1, #1
+        bne wait4
+		b	loop
 
+		;; Boucle d'attente pour la mise en grille
+WAITmg
+		ldr r1, =0xAFFFFF 
+wait5	
+		ldr r7, = GPIO_PORTE_BASE + (BROCHE0<<2)
+		ldr r10,[r7]
+		CMP r10,#0x00
+		BEQ B_actif_depart
+		
+		ldr r7, = GPIO_PORTE_BASE + (BROCHE1<<2)
+		ldr r10,[r7]
+		CMP r10,#0x00
+		BEQ B_actif_depart
 
-		   		
+		subs r1, #1
+        bne wait5
+		b	PLACEMENT
+		
+		;; Boucle d'attente pour le départ
+WAITstart
+		ldr r1, =0xAFFFFF 
+wait	
+		ldr r7, = GPIO_PORTD_BASE + (BROCHE7<<2)
+		ldr r10,[r7]
+		CMP r10,#0x00
+		BEQ SW2_actif
 
+		subs r1, #1
+        bne wait
+		b	WAITstart
+		
 		;; Boucle d'attente pour la marche avant
 WAIT	ldr r1, =0xAFFFFF 
 wait1	
@@ -168,7 +261,10 @@ wait1
 		CMP r10,#0x00
 		BEQ BG_actif
 		
-		
+		ldr r7, = GPIO_PORTD_BASE + (BROCHE6<<2)
+		ldr r10,[r7]
+		CMP r10,#0x00
+		BEQ SW1_actif
 
 		subs r1, #1
         bne wait1
@@ -197,6 +293,15 @@ WAITtourne
 wait3	
 		subs r1, #1
         bne wait3
+		
+		;; retour à la suite du lien de branchement
+		BX	LR
+		
+WAIToff		
+		ldr r7, = GPIO_PORTD_BASE + (BROCHE6<<2)
+		ldr r10,[r7]
+		CMP r10,#0x00
+		BEQ	loop
 		
 		;; retour à la suite du lien de branchement
 		BX	LR
